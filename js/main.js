05dd8c40eb9a013bd4c0e2360cb2a89fb66982f7
caronavirus <62344318+caronavirus@users.noreply.github.com>
2020-03-18 20:51:30 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.projects-pagination',
+        bulletClass: 'projects-bullet',
+        bulletActiveClass: 'projects-bullet-active',
+        clickable: true
+    },   
+});
